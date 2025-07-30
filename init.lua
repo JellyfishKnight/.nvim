19249aa8a146dd9eeb883d0870507d4ec9003874
JellyfishKnight <knightjellyfish@gmail.com>
2025-07-30 14:13:20 +0800

    从头开始配置

--- init.lua
+++ init.lua
@@ -1,5 +1,34 @@
-require('options')
-require('keymaps')
-require('plugins')
--- require('colorscheme')
-require("lsp")
+vim.g.mapleader = " "  -- 设置leader为空格
+vim.g.maplocalleader = " "  -- 设置本地leader为空格
+
+
+require("options")
+require("keymaps")
+
+-- 自动安装 lazy.nvim 插件管理器
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- vim.fn.stdpath("data") 获取 Neovim 数据目录路径
+-- 完整路径类似：~/.local/share/nvim/lazy/lazy.nvim
+
+if not vim.loop.fs_stat(lazy_path) then 
+    -- 如果 lazy.nvim 不存在，就自动下载安装
+    print("正在安装 lazy.nvim...")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",          -- 只克隆必要的文件，加快速度
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",             -- 使用稳定版本
+        lazypath,
+    })
+    print("lazy.nvim 安装完成！")
+end
+
+-- 将 lazy.nvim 添加到运行时路径
+vim.opt.rtp:prepend(lazy_path)
+-- rtp 是 runtimepath 的缩写，告诉 Neovim 在哪里找插件
+
+print("配置加载完成，lazy.nvim 已就绪！")
+
+-- Plugins需要在Lazy之后加载不然会找不到Lazy
+require("plugins")
--- lua/options.lua
+++ lua/options.lua
@@ -1,25 +1,19 @@
--- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = 'unnamedplus' -- use system clipboard
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
+local opt = vim.opt
 
--- Tab
-vim.opt.tabstop = 4 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4 -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
-vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+-- 开启行号显示
 
--- UI config
-vim.opt.number = true -- show absolute number
-vim.opt.relativenumber = true -- add numbers to each line on the left side
-vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true -- open new vertical split bottom
-vim.opt.splitright = true -- open new horizontal splits right
--- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
+opt.number = true
+opt.relativenumber = true
 
--- Searching
-vim.opt.incsearch = true -- search as characters are entered
-vim.opt.hlsearch = false -- do not highlight matches
-vim.opt.ignorecase = true -- ignore case in searches by default
-vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+-- 缩进相关
+opt.tabstop = 4    -- tab为几个空格
+opt.shiftwidth = 4     -- 自动锁紧的时候的tab长度
+opt.expandtab = true     -- 防止tab输入为制表符
+
+-- 搜索
+opt.ignorecase = true   -- 忽略大小写
+opt.smartcase = true    -- 如果搜索词包含大小写则区分大小写
+
+opt.mouse = "a" -- 启用鼠标
+
+
